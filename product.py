00083900c82637eafef17aa443e59dd728330e4b
sairaksrk <sairakswk@gmail.com>
2022-05-09 22:41:10 +0700

    new commit

--- product.py
+++ product.py
@@ -1,10 +1,1 @@
 print('coffee')
-<<<<<<< HEAD
-print('coffee2')
-print('coffee3')
-print('coffee4')
-print('coffee5')
-print('coffee6')
-print('coffee7')
-=======
->>>>>>> 8d3b17a6f7dfde93db9c05f69720b063ea6cec1f
